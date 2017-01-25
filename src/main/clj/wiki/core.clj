f6c0febfb8070a998589946d6494574bd09efa6c
hiroyuki nagata <newserver002@gmail.com>
2017-01-25 21:29:47 +0900

    Add routing without library

--- src/main/clj/wiki/core.clj
+++ src/main/clj/wiki/core.clj
@@ -4,10 +4,63 @@
 
 (defonce server (atom nil))
 
-(defn handler [req]
+;; ライブラリを使わないルーティング実装
+(defn ok [body]
   {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, World!!"})
+   :body body})
+
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
+
+(defn not-found []
+  {:status 404
+   :body "<h1>404 page not found</1>"})
+
+(defn home-view [req]
+  "<h1>ホーム画面</h1>
+   <a href=\"/wiki\">WIKI 一覧</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+(def wiki-index
+  [{:title "朝ごはんを作る"}
+   {:title "燃えるゴミを出す"}
+   {:title "卵を買って帰る"}
+   {:title "お風呂を洗う"}])
+
+(defn wiki-index-view [req]
+  `("<h1>WIKI 一覧</h1>"
+    "<ul>"
+    ~@(for [{:keys [title]} wiki-index]
+        (str "<li>" title "</li>"))
+    "</ul>"))
+
+(defn wiki-index [req]
+  (-> (wiki-index-view req)
+      ok
+      html))
+
+(def routes
+  {"/" home
+   "/wiki" wiki-index})
+
+(defn match-route [uri]
+  (get routes uri))
+
+;(defn handler [req]
+;  {:status 200
+;   :headers {"Content-Type" "text/plain"}
+;   :body "Hello, World!!"})
+
+(defn handler [req]
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+      (maybe-fn req)
+      (not-found))))
 
 (defn start-server [& {:keys [host port join?]
                        :or {host "localhost" port 3000 join? false}}]
