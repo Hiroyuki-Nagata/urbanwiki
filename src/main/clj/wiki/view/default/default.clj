90d498b7c0d99f47badc5eb56a39fa0415335f89
hiroyuki nagata <newserver002@gmail.com>
2017-01-31 23:24:45 +0900

    Add css file

--- src/main/clj/wiki/view/default/default.clj
+++ src/main/clj/wiki/view/default/default.clj
@@ -1,7 +1,7 @@
 (ns wiki.view.default.default
   (:require [hiccup.page :refer [html5 include-css include-js]]))
 
-(defn default-header []
+(defn default-header [title]
   [:head
    [:meta {:name "ROBOTS" :content "NOINDEX, NOFOLLOW"}]
    [:meta {:name "ROBOTS" :content "NOARCHIVE"}]
@@ -9,18 +9,19 @@
    [:meta {:http-equiv "Content-Style-Type" :content "text/css"}]
    [:link {:rel "stylesheet" :type "text/css" :href ""}]
    [:style {:type "text/css"}]
-   [:title "FreeStyleWiki"]]
-  )
+   [:title title]
+   (include-css "/css/default/default.css")
+   ])
 
 (defn top [req & body]
   (html5
-   (default-header)
+   (default-header "FreeStyleWiki - top")
    [:body
     [:header.top-bar.bg-green.depth-3 "FreeStyleWiki!"]
     [:main body]]))
 
 (defn common [req]
   (html5
-   (default-header)
+   (default-header "FreeStyleWiki - wiki")
    [:body
     [:header.top-bar.bg-green.depth-3 "FreeStyleWiki!"]]))
