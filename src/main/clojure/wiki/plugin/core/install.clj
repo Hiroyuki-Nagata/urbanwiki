d24f814fc5e7b1c3ac7fc101d57d150d7e01f950
hiroyuki nagata <newserver002@gmail.com>
2017-02-03 00:57:20 +0900

    Merging

--- src/main/clojure/wiki/plugin/core/install.clj
+++ src/main/clojure/wiki/plugin/core/install.clj
@@ -4,6 +4,9 @@
    clojure.tools.logging
    clj-logging-config.log4j))
 
+; TODO: wikiのインスタンスを渡す必要がある
 (defn install []
+  ; 呼び出しイメージ
+  ; (. wiki add-menu "トップ" "URL" 999)
   (set-logger!)
   (info "Install core plugin..."))
--- src/main/clojure/wiki/wiki.clj
+++ src/main/clojure/wiki/wiki.clj
@@ -1,7 +1,20 @@
 (ns wiki.wiki
+  (:gen-class
+   :init init
+   :prefix "-"
+   :main false)
   (:require [compojure.core :refer [defroutes context GET]]
             [compojure.route :as route]
             [wiki.view.default.default :as default]))
+
+(defn -init []
+  [[] (atom {:menu []})])
+
+; メニュー項目を追加します。既に同じ名前の項目が登録されている場合は上書きします。
+; 優先度が高いほど左側に表示されます。
+; add-menu(項目名,URL,優先度,クロールを拒否するかどうか)
+(defn add-menu [name href weight nofollow]
+  (setfield this :menu (conj )))
 
 (defn ok [body]
   {:status 200
