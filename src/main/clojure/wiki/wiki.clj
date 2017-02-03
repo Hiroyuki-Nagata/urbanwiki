681f51de9c37682dfe4992f2414dd27df9d929df
hiroyuki nagata <newserver002@gmail.com>
2017-02-03 23:32:20 +0900

    Implement state & atom based config

--- src/main/clojure/wiki/wiki.clj
+++ src/main/clojure/wiki/wiki.clj
@@ -12,21 +12,54 @@
 ;;
 ;; Singleton pattern
 ;; http://mishadoff.com/blog/clojure-design-patterns/#singleton
+;; http://stackoverflow.com/a/3268284/2565527
 ;;
 ;; USE:
 ;; https://github.com/levand/immuconf
 ;;
+(def wiki-state (atom {}))
+(defonce wiki-config-rsc "resources/config.edn")
+
+(defn clear-state []
+  (swap! wiki-state (fn [p] {})))
+
+(defn get-state
+  ([] @wiki-state)
+  ([key] (@wiki-state key)))
+
+(defn update-state [key val]
+  (swap! wiki-state assoc key val))
+
+(defn load-default-config []
+  (cfg/load wiki-config-rsc))
+
 ;; グローバルなコンフィグ読み出し
 (defn load-config
-  ([] (let [conf (cfg/load "resources/config.edn")]
+  ;; 引数なし
+  ([] (let [conf (get-state)]
         (set-logger!)
-        (debug (str "load-config: " conf) )))
-  ([& args] (let [conf (get-in (cfg/load "resources/config.edn") args)]
-              (set-logger!)
-              (debug (str "load-config: " conf)))))
+        (debug (str "load-config: " conf))
+        conf))
+  ;; 引数あり
+  ([& args] (let [conf (get-in (get-state) args)]
+        (set-logger!)
+        (debug (str "load-config: " conf))
+        conf)))
 
 ;; グローバルなコンフィグ書き込み
-(defn save-config [])
+(defn save-config [m]
+  (set-logger!)
+  (debug (str "save-config: " m))
+  (let [updated (merge m (get-state))]
+    (doseq [[k v] updated] (update-state k v))))
+
+;; コンフィグの初期値を読み出す
+(defn init-config []
+  (set-logger!)
+  (clear-state)
+  (doseq [[k v] (cfg/load wiki-config-rsc)] (update-state k v))
+  (debug (str "init-config: " @wiki-state))
+  @wiki-state)
 
 ;; メニュー項目を追加します。既に同じ名前の項目が登録されている場合は上書きします。
 ;; 優先度が高いほど左側に表示されます。
