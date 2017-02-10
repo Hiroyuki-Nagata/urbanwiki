1bf45d54bbce93cb2b9e7635f0a3cad7a0e01f9f
hiroyuki nagata <newserver002@gmail.com>
2017-02-11 00:50:17 +0900

    Add some menu items

--- src/main/clojure/wiki/plugin/core/install.clj
+++ src/main/clojure/wiki/plugin/core/install.clj
@@ -11,4 +11,8 @@
   (set-logger!)
   (info "Install core plugin...")
   ;; トップページ
-  (wiki/add-menu "トップ" (wiki/create-page-url (db/load-config :frontpage)) 999 nil))
+  (wiki/add-menu "トップ" (wiki/create-page-url (db/load-config :frontpage)) 999 nil)
+  (wiki/add-menu "新規"   (wiki/create-url {:action "NEW"})                  998 1)
+  (wiki/add-menu "編集"   (wiki/create-url {:action ""})                     997 1)
+  (wiki/add-menu "一覧"   (wiki/create-url {:action "LIST"})                 995 nil)
+  (wiki/add-menu "ヘルプ" (wiki/create-page-url "Help")                      100 nil))
