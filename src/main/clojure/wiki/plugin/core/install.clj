df619124e151b0cc9e6d5588bdd7748dca7c30b4
hiroyuki nagata <newserver002@gmail.com>
2017-02-12 00:46:46 +0900

    Append required handlers & hooks for core plugin

--- src/main/clojure/wiki/plugin/core/install.clj
+++ src/main/clojure/wiki/plugin/core/install.clj
@@ -1,3 +1,6 @@
+;;
+;; FreeStyleWikiの基本的な機能を提供します。
+;;
 (ns wiki.plugin.core.install
   (:gen-class install true)
   (:use
@@ -14,4 +17,14 @@
   (wiki/add-menu "新規"   (wiki/create-url {:action "NEW"})                  998 1)
   (wiki/add-menu "編集"   (wiki/create-url {:action ""})                     997 1)
   (wiki/add-menu "一覧"   (wiki/create-url {:action "LIST"})                 995 nil)
-  (wiki/add-menu "ヘルプ" (wiki/create-page-url "Help")                      100 nil))
+  (wiki/add-menu "ヘルプ" (wiki/create-page-url "Help")                      100 nil)
+
+  ;;(wiki/add-handler ""     "plugin.core.show-page")
+  ;;(wiki/add-handler "NEW"  "plugin.core.new-page" )
+  ;;(wiki/add-handler "LIST" "plugin.core.list-page")
+  ;;(wiki/add-handler "EDIT" "plugin.core.edit-page")
+  ;;(wiki/add-hook    "show" "plugin.core.edit-page")
+  ;;(wiki/add-handler "DIFF" "plugin.core.diff"     )
+  ;;(wiki/add-hook    "show" "plugin.core.diff"     )
+
+  )
