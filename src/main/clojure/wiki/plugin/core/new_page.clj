c8b3499eda4552ec8641867f0c5bc831090d54f9
hiroyuki nagata <newserver002@gmail.com>
2017-03-05 02:54:45 +0900

    Change raw HTML to hiccup

--- src/main/clojure/wiki/plugin/core/new_page.clj
+++ src/main/clojure/wiki/plugin/core/new_page.clj
@@ -2,12 +2,11 @@
   (:gen-class true)
   (:use
    clojure.tools.logging)
-  (:require [wiki.default-storage :as db]
+  (:require [hiccup.page]
             [wiki.wiki :as wiki]))
 
 (defn do-action [req]
-  (str "<form method=\"post\" action=\"" (wiki/create-url) "\">\n
-    <input type=\"text\" name=\"page\" size=\"40\">
-    <input type=\"submit\" value=\" 作成 \">
-    <input type=\"hidden\" name=\"action\" value=\"EDIT\">
-  </form>\n"))
+  [:form {:method "post" :action (wiki/create-url)}
+   [:input {:type "text" :name "page" :size "40"}]
+   [:input {:type "submit" :value "作成"}]
+   [:input {:type "hidden" :name "action" :value "EDIT"}]])
