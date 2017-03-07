c7ea48c04255468bd5417e7d871355805cb5b60e
hiroyuki.nagata <idiotpanzer@gmail.com>
2017-03-07 13:33:05 +0900

    Append login form

--- src/main/clojure/wiki/plugin/admin/login.clj
+++ src/main/clojure/wiki/plugin/admin/login.clj
@@ -1,15 +1,17 @@
 (ns wiki.plugin.admin.login
   (:gen-class true)
   (:use
-   clojure.tools.logging)
+   clojure.tools.logging
+   wiki.view.login)
   (:require [clojure.string :refer [blank?]]
             [wiki.default-storage :as db]
             [wiki.wiki :as wiki]))
 
-(defn default []
+(defn default [req]
   (debug "Called login/default")
-  "Hello")
+  (let [page-name (:page (wiki/params))]
+    (login-tmpl "xxx" page-name)))
 
 (defn do-action [req]
   (debug "Called login/do-action")
-  (default))
+  (default req))
---
+++ src/main/clojure/wiki/view/login.clj
@@ -0,0 +1,19 @@
+(ns wiki.view.login
+  (:use [clojure.string])
+  (:require [hiccup.page]))
+
+(defn login-tmpl [url page]
+  [:h2 "IDとパスワードを入力してください"]
+  [:form {:action url :method "post"}
+   [:table
+    [:tr
+     [:th "ID"]
+     [:td
+      [:input {:type "text" :name "id"}]]]
+    [:tr
+     [:th "Pass"]
+     [:td [:input {:type "password" :name "pass"}]]]]
+   [:br]
+   [:input {:type "submit" :name "login" :value "ログイン"}]
+   [:input {:type "hidden" :name "action" :value "LOGIN"}]
+   [:input {:type "hidden" :name "page" :value page}]])
