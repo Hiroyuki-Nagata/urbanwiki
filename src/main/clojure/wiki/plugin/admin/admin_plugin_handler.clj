e0ffd8ada51a4f1b137d95a5720f1b35afa25763
hiroyuki nagata <newserver002@gmail.com>
2017-03-11 01:08:15 +0900

    Implement plugin management view on a half way

--- src/main/clojure/wiki/plugin/admin/admin_plugin_handler.clj
+++ src/main/clojure/wiki/plugin/admin/admin_plugin_handler.clj
@@ -5,6 +5,15 @@
             [wiki.default-storage :as db]
             [wiki.wiki :as wiki]))
 
+(defn list-plugins []
+  ;; 可読性が悪い…が、まず "wiki.plugin" 以下の名前空間を取得している
+  ;; その後、そこから "wiki.plugin.XXX" のうち "XXX" を取り出して重複を削除して返す
+  (let [plugins (filter #(str/starts-with? (str (ns-name %)) "wiki.plugin.") (all-ns))
+        plugin-ns (distinct (map #(nth (str/split (str %) #"\.") 2 nil) plugins))]
+    (debug (pr-str "Installed plugin sources --> " plugins))
+    (info  (pr-str "Installed plugin namespaces --> " plugin-ns))
+    plugin-ns))
+
 (defn save-plugin-config [req]
   (let [plugins (:plugin (wiki/params))]
     (db/save-config {:plugins plugins})
@@ -18,11 +27,18 @@
      [:tr
       [:th [:br]]
       [:th "プラグイン"]
-      [:th "説明"]]]
-    ;; <-- ここにプラグインのリスト取得処理を書く
-    [:input {:type "submit" :name "save"   :value "保存"}]
-    [:input {:type "reset"                 :value "リセット"}]
-    [:input {:type "hidden" :name "action" :value "ADMINPLUGIN"}]]])
+      [:th "説明"]]
+     (for [plugin (list-plugins)]
+       [:tr
+        [:td
+         [:input {:type "checkbox" :name "plugin" :value (str plugin)}]]
+        [:td
+         (str plugin)]
+        [:td
+         "説明"]])
+     [:input {:type "submit" :name "save"   :value "保存"}]
+     [:input {:type "reset"                 :value "リセット"}]
+     [:input {:type "hidden" :name "action" :value "ADMINPLUGIN"}]]]])
 
 (defn do-action [req]
   (if (some? (:save (wiki/params)))
